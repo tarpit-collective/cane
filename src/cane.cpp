7dda1acb4b310379392ba105dd18a126072044b5
Jack Clarke <jackojc@gmail.com>
2024-04-26 00:40:50 +0100

    Implement logging and some basic error/symbol/logging kinds

--- src/cane.cpp
+++ src/cane.cpp
@@ -8,282 +8,330 @@
 #include <filesystem>
 #include <memory>
 
-extern "C" {
-	#include <jack/jack.h>
-	#include <jack/midiport.h>
-	#include <jack/ringbuffer.h>
-}
+#include <libremidi/libremidi.hpp>
+#include <libremidi/reader.hpp>
 
-#include <lib.hpp>
 #include <conflict/conflict.hpp>
 
-struct jack_deleter {
-	template <typename T> constexpr void operator()(T arg) const {
-		jack_free(arg);
+#include <fmt/core.h>
+#include <fmt/format.h>
+#include <fmt/ranges.h>
+#include <fmt/color.h>
+#include <fmt/ostream.h>
+
+#include <commandline.h>
+
+// Macros
+namespace cane {
+#define CANE_STR_IMPL_(x) #x
+#define CANE_STR(x)       CANE_STR_IMPL_(x)
+
+#define CANE_CAT_IMPL_(x, y) x##y
+#define CANE_CAT(x, y)       CANE_CAT_IMPL_(x, y)
+
+#define CANE_VAR(x) CANE_CAT(var_, CANE_CAT(x, CANE_CAT(__LINE__, _)))
+
+#define CANE_TRACE "[" __FILE__ ":" CANE_STR(__LINE__) "] "
+}  // namespace cane
+
+// Utilities
+namespace pv {
+	template <typename T, typename... Ts>
+	[[nodiscard]] constexpr bool any(T&& first, Ts&&... rest) {
+		return ((std::forward<T>(first) or std::forward<Ts>(rest)) or ...);
 	}
-};
 
-using JackPorts = std::unique_ptr<const char*[], jack_deleter>;
+	template <typename T, typename... Ts>
+	[[nodiscard]] constexpr bool all(T&& first, Ts&&... rest) {
+		return ((std::forward<T>(first) and std::forward<Ts>(rest)) and ...);
+	}
 
-enum {
-	OPT_HELP = 0b001,
-	OPT_LIST = 0b010,
-};
+	template <typename T, typename... Ts>
+	[[nodiscard]] constexpr bool none(T&& first, Ts&&... rest) {
+		return ((not std::forward<T>(first) and not std::forward<Ts>(rest)) and ...);
+	}
 
-inline std::string read_file(std::filesystem::path path) {
-	try {
-		std::filesystem::path cur = path;
+	template <typename T, typename... Ts>
+	[[nodiscard]] constexpr bool eq_all(T&& first, Ts&&... rest) {
+		return ((std::forward<T>(first) == std::forward<Ts>(rest)) and ...);
+	}
 
-		while (std::filesystem::is_symlink(cur)) {
-			std::filesystem::path tmp = std::filesystem::read_symlink(cur);
+	template <typename T, typename... Ts>
+	[[nodiscard]] constexpr bool eq_any(T&& first, Ts&&... rest) {
+		return ((std::forward<T>(first) == std::forward<Ts>(rest)) or ...);
+	}
 
-			if (tmp == cur)
-				cane::general_error(cane::STR_SYMLINK_ERROR, path.string());
+	template <typename T, typename... Ts>
+	[[nodiscard]] constexpr bool eq_none(T&& first, Ts&&... rest) {
+		return ((std::forward<T>(first) != std::forward<Ts>(rest)) and ...);
+	}
+}  // namespace pv
 
-			cur = tmp;
+// Errors
+namespace cane {
+#define CANE_RESET "\x1b[0m"
+#define CANE_BOLD  "\x1b[1m"
+
+#define CANE_BLACK   "\x1b[30m"
+#define CANE_RED     "\x1b[31m"
+#define CANE_GREEN   "\x1b[32m"
+#define CANE_YELLOW  "\x1b[33m"
+#define CANE_BLUE    "\x1b[34m"
+#define CANE_MAGENTA "\x1b[35m"
+#define CANE_CYAN    "\x1b[36m"
+#define CANE_WHITE   "\x1b[37m"
+
+#define CANE_ERR CANE_RED CANE_BOLD
+#define CANE_OK  CANE_BLUE
+}  // namespace cane
+
+namespace cane {
+#define LOG_KINDS \
+	X(INFO, CANE_RESET "[-]") \
+	X(WARN, CANE_BLUE "[*]") \
+	X(FAIL, CANE_RED "[!]") \
+	X(OKAY, CANE_GREEN "[^]") \
+	X(EXPR, CANE_MAGENTA "[=]")
+
+#define X(a, b) a,
+	enum class LogKind : size_t {
+		LOG_KINDS
+	};
+#undef X
+
+	namespace detail {
+#define X(a, b) std::string_view { b },
+		constexpr std::array LOG_TO_STR = { LOG_KINDS };
+#undef X
+
+		constexpr std::string_view log_to_str(LogKind x) {
+			return LOG_TO_STR[static_cast<size_t>(x)];
 		}
-
-		if (std::filesystem::is_directory(cur) or std::filesystem::is_other(cur))
-			cane::general_error(cane::STR_NOT_FILE_ERROR, path.string());
-
-		if (not std::filesystem::exists(cur))
-			cane::general_error(cane::STR_FILE_NOT_FOUND_ERROR, path.string());
-
-		std::ifstream is(cur);
-
-		if (not is.is_open())
-			cane::general_error(cane::STR_FILE_READ_ERROR, path.string());
-
-		std::stringstream ss;
-		ss << is.rdbuf();
-
-		return ss.str();
 	}
 
-	catch (const std::filesystem::filesystem_error&) {
-		cane::general_error(cane::STR_FILE_READ_ERROR, path.string());
+	inline std::ostream& operator<<(std::ostream& os, LogKind x) {
+		return (os << detail::log_to_str(x));
 	}
-}
+}  // namespace cane
 
-int main(int argc, const char* argv[]) {
-	std::string_view device;
-	std::string_view filename;
-	uint64_t flags;
+template <>
+struct fmt::formatter<cane::LogKind>: fmt::ostream_formatter {};
 
-	auto parser = conflict::parser {
-		conflict::option { { 'h', "help", "show help" }, flags, OPT_HELP },
-		conflict::option { { 'l', "list", "list available midi devices" }, flags, OPT_LIST },
+namespace cane {
+	namespace detail {
+		template <typename... Ts>
+		constexpr decltype(auto) log_impl(LogKind kind,
+			std::string_view file,
+			std::string_view line,
+			std::string_view func,
+			std::string_view fmt,
+			Ts&&... args) {
+			fmt::print(stderr, "{} [{}:{}] ", kind, std::filesystem::relative(file).native(), line);
 
-		conflict::string_option { { 'f', "file", "input file" }, "filename", filename },
-		conflict::string_option { { 'm', "midi", "midi device to connect to" }, "device", device }
+			if (func != "operator()") {
+				fmt::print(stderr, "`{}`" CANE_RESET, func);
+			}
+
+			fmt::print(stderr, " ");
+			fmt::print(stderr, fmt::runtime(fmt), std::forward<Ts>(args)...);
+
+			fmt::print(stderr, "\n");
+		}
+	}  // namespace detail
+
+	namespace detail {
+		template <typename T>
+		inline decltype(auto) dbg_impl(
+			std::string_view file, std::string_view line, std::string_view func, std::string_view str, T&& expr) {
+			fmt::print(std::cerr,
+				"{} [{}:{}] `{}`" CANE_RESET " {} = {}\n",
+				LogKind::EXPR,
+				std::filesystem::relative(file).native(),
+				line,
+				func,
+				str,
+				std::forward<T>(expr));
+
+			return std::forward<T>(expr);
+		}
+	}
+
+#ifndef NDEBUG
+
+#define CANE_DBG(expr) \
+	[&, CANE_VAR(func) = __func__]() { \
+		return cane::detail::dbg_impl(__FILE__, CANE_STR(__LINE__), CANE_VAR(func), CANE_STR(expr), (expr)); \
+	}()
+
+#define CANE_DBG_RUN(expr) \
+	do { \
+		((expr)); \
+	} while (0)
+
+#define CANE_LOG(...) \
+	do { \
+		[CANE_VAR(func) = __func__]<typename... Ts>( \
+			cane::LogKind CANE_VAR(kind), std::string_view fmt, Ts&&... CANE_VAR(args)) { \
+			cane::detail::log_impl(CANE_VAR(kind), \
+				__FILE__, \
+				CANE_STR(__LINE__), \
+				CANE_VAR(func), \
+				fmt, \
+				std::forward<Ts>(CANE_VAR(args))...); \
+		}(__VA_ARGS__); \
+	} while (0)
+
+#else
+
+#define CANE_DBG(expr) ((expr))
+
+#define CANE_DBG_RUN(expr) \
+	do { \
+	} while (0)
+
+#define CANE_LOG(...) \
+	do { \
+	} while (0)
+
+#endif
+
+}  // namespace cane
+
+namespace cane {
+	// Error types
+#define ERROR_KINDS \
+	X(GENERIC, "an error occurred") \
+	X(UNREACHABLE, "unreachable code") \
+	X(NOT_IMPLEMENTED, "not implemented") \
+	X(UNKNOWN_CHAR, "unknown character") \
+	X(UNDEFINED, "undefined") \
+\
+	X(UNEXPECTED_TOKEN, "unexpected token") \
+	X(INVALID_AST, "invalid tree") \
+\
+	X(UNKNOWN_SYMBOL, "unknown symbol")
+
+#define X(a, b) a,
+	enum class ErrorKind : size_t {
+		ERROR_KINDS
+	};
+#undef X
+
+	namespace detail {
+#define X(a, b) std::string_view { b },
+		constexpr std::array ERROR_TO_STR = { ERROR_KINDS };
+#undef X
+
+		constexpr std::string_view error_to_str(ErrorKind x) {
+			return ERROR_TO_STR[static_cast<size_t>(x)];
+		}
+	}  // namespace detail
+
+	inline std::ostream& operator<<(std::ostream& os, ErrorKind x) {
+		return (os << detail::error_to_str(x));
+	}
+}  // namespace cane
+
+template <>
+struct fmt::formatter<cane::ErrorKind>: fmt::ostream_formatter {};
+
+namespace cane {
+	struct Report {
+		std::string_view sv;
+		ErrorKind kind;
 	};
 
-	parser.apply_defaults();
-	auto status = parser.parse(argc - 1, argv + 1);
-
-	try {
-		// Handle argument parsing errors.
-		switch (status.err) {
-			case conflict::error::invalid_option:
-				cane::general_error(cane::STR_OPT_INVALID_OPTION, status.what1);
-
-			case conflict::error::invalid_argument:
-				cane::general_error(cane::STR_OPT_INVALID_ARG, status.what1, status.what2);
-
-			case conflict::error::missing_argument:
-				cane::general_error(cane::STR_OPT_MISSING_ARG, status.what1);
-
-			case::conflict::error::ok:
-				break;
-		}
-
-		if (flags & OPT_HELP) {
-			parser.print_help();
-			return 0;
-		}
-
-		// Setup JACK
-		using namespace std::chrono_literals;
-
-		struct JackData {
-			jack_client_t* client = nullptr;
-			jack_port_t* port = nullptr;
-
-			jack_nframes_t sample_rate = 0;
-			jack_nframes_t buffer_size = 0;
-			uint64_t time = 0u;
-
-			cane::Timeline::iterator it;
-			cane::Timeline::const_iterator end;
-
-			cane::Timeline events;
-
-			~JackData() {
-				if (client != nullptr)
-					jack_deactivate(client);
-
-				if (port != nullptr)
-					jack_port_unregister(client, port);
-
-				if (client != nullptr)
-					jack_client_close(client);
-			}
-		} midi {};
-
-
-		// Connect to JACK
-		if (not (midi.client = jack_client_open(cane::CSTR_EXE, JackOptions::JackNoStartServer, nullptr)))
-			cane::general_error(cane::STR_CONNECT_ERROR);
-
-
-		// Sample rate changed callback. We use the sample rate to determine timing
-		// information so this is crucial.
-		if (jack_set_sample_rate_callback(midi.client, [] (jack_nframes_t sample_rate, void* arg) {
-			JackData& midi = *static_cast<JackData*>(arg);
-			midi.sample_rate = sample_rate;
-			return 0;
-		}, static_cast<void*>(&midi)))
-			cane::general_error(cane::STR_SAMPLE_RATE_CALLBACK_ERROR);
-
-
-		// Notify of buffer size changes
-		if (jack_set_buffer_size_callback(midi.client, [] (jack_nframes_t buffer_size, void* arg) {
-			JackData& midi = *static_cast<JackData*>(arg);
-			midi.buffer_size = buffer_size;
-			return 0;
-		}, static_cast<void*>(&midi)))
-			cane::general_error(cane::STR_BUFFER_SIZE_CALLBACK_ERROR);
-
-		// MIDI out callback
-		if (jack_set_process_callback(midi.client, [] (jack_nframes_t nframes, void *arg) {
-			auto& [client, port, sample_rate, buffer_size, time, it, end, events] = *static_cast<JackData*>(arg);
-
-			void* out_buffer = jack_port_get_buffer(port, nframes);
-			jack_midi_clear_buffer(out_buffer);
-
-			// Copy every MIDI event into the buffer provided by JACK.
-			for (; it != end and it->time <= time; ++it) {
-				if (jack_midi_event_write(out_buffer, 0, it->data.data(), it->data.size()))
-					cane::general_error(cane::STR_WRITE_ERROR);
-			}
-
-			size_t lost = 0;
-			if ((lost = jack_midi_get_lost_event_count(out_buffer)))
-				cane::general_warning(cane::STR_LOST_EVENT, lost);
-
-			time += cane::SECOND / (sample_rate / nframes);
-
-			return 0;
-		}, static_cast<void*>(&midi)))
-			cane::general_error(cane::STR_PROCESS_CALLBACK_ERROR);
-
-
-		// If no device is specified _and_ `-l` is not passed,
-		// throw an error. It's perfectly valid to give an empty
-		// string to JACK here and it will give us back a list
-		// of ports regardless.
-		if (device.empty() and (flags & OPT_LIST) != OPT_LIST)
-			cane::general_error(cane::STR_NO_DEVICE);
-
-
-		// Register port and get information.
-		if (not (midi.port = jack_port_register(midi.client, cane::CSTR_PORT, JACK_DEFAULT_MIDI_TYPE, JackPortIsOutput, 0)))
-			cane::general_error(cane::STR_PORT_ERROR);
-
-		midi.buffer_size = jack_get_buffer_size(midi.client);
-		midi.sample_rate = jack_get_sample_rate(midi.client);
-
-
-		// Get an array of all MIDI input ports that we could potentially connect to.
-		JackPorts ports { jack_get_ports(midi.client, std::string { device }.c_str(), JACK_DEFAULT_MIDI_TYPE, JackPortIsInput) };
-
-		if (not ports)  // Error occured
-			cane::general_error(cane::STR_GET_PORTS_ERROR);
-
-		if (not ports[0])  // No MIDI input ports.
-			cane::general_error(cane::STR_NOT_FOUND, device);
-
-		// Print all devices if list option passed
-		if (flags & OPT_LIST) {
-			for (size_t i = 0; ports[i] != nullptr; ++i)
-				cane::general_notice(cane::STR_DEVICE, ports[i]);
-
-			return 0;
-		}
-
-		if (jack_connect(midi.client, jack_port_name(midi.port), ports[0]))
-			cane::general_error(cane::STR_PATCH_ERROR);
-
-
-		// Compiler
-		if (filename.empty())
-			cane::general_error(cane::STR_OPT_NO_FILE);
-
-		std::string in = read_file(filename);
-		cane::View src { &*in.begin(), &*in.end() };
-
-		namespace time = std::chrono;
-
-		using clock = time::steady_clock;
-		using unit = time::microseconds;
-
-		// Compile
-		auto t1 = clock::now();
-		cane::Timeline timeline = cane::compile(src, cane::default_handler);
-		auto t2 = clock::now();
-
-		if (timeline.empty())
-			return 0;
-
-		CANE_DBG_RUN(cane::print(std::cerr, timeline));
-		CANE_LOG(cane::LogLevel::DBG, "event(s) = ", timeline.size());
-		CANE_LOG(cane::LogLevel::DBG, "events/s = ", timeline.size() / (timeline.duration / cane::SECOND));
-
-		// Setup MIDI events.
-		// Very important that we assign these here or else
-		// the sequencer will not run, or worse- start
-		// sequencing garbage values.
-		midi.events = timeline;
-
-		midi.it = timeline.begin();
-		midi.end = timeline.cend();
-
-		// Call this or else our callback is never called.
-		if (jack_activate(midi.client))
-			cane::general_error(cane::STR_ACTIVATE_ERROR);
-
-		// Sleep until timeline is completed.
-		size_t count = 1;
-		size_t barw = 50;
-
-		while (midi.it != midi.end) {
-			cane::print(std::cout, "\r", CANE_BOLD, count, "% [");
-
-			for (size_t i = 0; i != barw; ++i) {
-				size_t perc = count / (100 / barw);
-
-				if (i < perc)       cane::print(std::cout, CANE_YELLOW "=");
-				else if (i == perc) cane::print(std::cout, CANE_YELLOW ">");
-				else                cane::print(std::cout, CANE_BLUE   "-");
-			}
-
-			auto so_far = ((timeline.duration / 100) * count) / cane::SECOND;
-			auto total = timeline.duration / cane::SECOND;
-
-			std::cout << std::fixed << std::setprecision(2);
-			cane::print(std::cout, CANE_RESET CANE_BOLD "] ", so_far, "s/", total, "s" CANE_RESET);
-			std::cout.flush();
-
-			count++;
-			std::this_thread::sleep_for(std::chrono::microseconds{timeline.duration / 100});
-		}
-
-		cane::println(std::cout);
+	[[noreturn]] inline void report(std::string_view sv, ErrorKind x) {
+		throw Report { sv, x };
 	}
 
-	catch (cane::Error) {
-		return 1;
+	[[noreturn]] inline void report(ErrorKind x) {
+		throw Report { "", x };
 	}
+
+	inline void report_handler(Report x) {
+		(x.sv.empty() or x.sv == "") ? fmt::print(stderr, "[" CANE_ERR "error" CANE_RESET "] {}\n", x.kind) :
+									   fmt::print(stderr, "[" CANE_ERR "error" CANE_RESET "] {}: `{}`\n", x.kind, x.sv);
+	}
+}  // namespace cane
+
+// Lexer
+namespace cane {
+	// Token types
+#define SYMBOL_KINDS \
+	X(NONE, "None") \
+	X(TERM, "Eof") \
+\
+	X(IDENTIFIER, "Identifier") \
+	X(INTEGER, "Integer") \
+\
+	X(BEAT, "Beat") \
+	X(REST, "Rest") \
+	X(RAND, "Rand") \
+\
+	X(END, "End")
+
+#define X(a, b) a,
+	enum class SymbolKind : size_t {
+		SYMBOL_KINDS
+	};
+#undef X
+
+	namespace detail {
+#define X(a, b) std::string_view { b },
+		constexpr std::array SYMBOL_TO_STR = { SYMBOL_KINDS };
+#undef X
+
+		constexpr std::string_view symbol_to_str(SymbolKind x) {
+			return detail::SYMBOL_TO_STR[static_cast<size_t>(x)];
+		}
+	}  // namespace detail
+
+	inline std::ostream& operator<<(std::ostream& os, SymbolKind x) {
+		return (os << detail::symbol_to_str(x));
+	}
+}  // namespace cane
+
+template <>
+struct fmt::formatter<cane::SymbolKind>: fmt::ostream_formatter {};
+
+namespace cane {
+	struct Symbol {
+		std::string_view sv;  // original token string
+		SymbolKind kind;
+
+		constexpr Symbol(): sv(""), kind(SymbolKind::NONE) {}
+
+		constexpr Symbol(SymbolKind kind_): sv(""), kind(kind_) {}
+
+		constexpr Symbol(std::string_view sv_, SymbolKind kind_): sv(sv_), kind(kind_) {}
+
+		bool operator==(const Symbol&) const = default;
+	};
+
+	struct Lexer {
+		std::string_view sv;  // remaining input from source
+
+		Symbol peek;
+		Symbol prev;
+
+		Lexer(std::string_view sv_): sv(sv_), peek(), prev() {}
+
+		template <typename F>
+		constexpr void expect(F&& fn, ErrorKind x) {
+			if (not fn(peek)) {
+				report(peek.sv, x);
+			}
+		}
+	};
+
+}  // namespace cane
+
+int main(int, const char*[]) {
+	CANE_LOG(cane::LogKind::OKAY, "fool");
+
+	auto x = CANE_DBG((2 + 2) + 1);
+	CANE_DBG(x + x);
 
 	return 0;
 }
