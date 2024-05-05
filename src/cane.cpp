bbd6079c1478bf4ea920bf1c669f0d8f814f22fb
Jack Clarke <jackojc@gmail.com>
2024-05-05 02:09:28 +0100

    Beginning work on parser

--- src/cane.cpp
+++ src/cane.cpp
@@ -201,17 +201,18 @@ namespace cane {
 #define ERROR_KINDS \
 	X(GENERIC, "an error occurred") \
 	X(UNREACHABLE, "unreachable code") \
-	X(NOT_IMPLEMENTED, "not implemented") \
-	X(UNKNOWN_CHAR, "unknown character") \
 	X(UNDEFINED, "undefined") \
+	X(NOT_IMPLEMENTED, "not implemented") \
 \
-	X(UNEXPECTED_TOKEN, "unexpected token") \
 	X(INVALID_AST, "invalid tree") \
+	X(UNKNOWN_SYMBOL, "unknown symbol") \
 \
-	X(EXPECTED_IDENT, "expected an identifier") \
-	X(EXPECTED_NUMBER, "expected a number") \
+	X(UNEXPECTED_CHAR, "unknown character") \
+	X(UNEXPECTED_TOKEN, "unexpected token") \
 \
-	X(UNKNOWN_SYMBOL, "unknown symbol")
+	X(EXPECTED_OPERATOR, "expected an operator") \
+	X(EXPECTED_IDENTIFIER, "expected an identifier") \
+	X(EXPECTED_NUMBER, "expected a number")
 
 #define X(a, b) a,
 	enum class ErrorKind : size_t {
@@ -259,62 +260,91 @@ namespace cane {
 			return;
 		}
 
+		if (sv.front() == '\0') {
+			fmt::print(stderr, "{}" CANE_RESET " {}: `EOF`!\n", LogKind::FAIL, kind);
+			return;
+		}
+
 		fmt::print(stderr, "{}" CANE_RESET " {}: `{}`!\n", LogKind::FAIL, kind, sv);
 	}
 }  // namespace cane
 
 // Lexer
 namespace cane {
+	enum class PrecKind : size_t {
+		NONE,
+		LAST,
+		INCR,
+	};
+
+	enum class AssKind : size_t {
+		NONE,
+		LEFT,
+		RIGHT,
+	};
+
 	// Token types
 #define SYMBOL_KINDS \
-	X(NONE, "None") \
-	X(ENDFILE, "Eof") \
-	X(WHITESPACE, "Whitespace") \
+	X(NONE, "None", NONE, NONE) \
+	X(ENDFILE, "Eof", NONE, NONE) \
+	X(WHITESPACE, "Whitespace", NONE, NONE) \
 \
-	X(IDENTIFIER, "Identifier") \
-	X(NUMBER, "Number") \
+	X(IDENTIFIER, "Identifier", NONE, NONE) \
+	X(NUMBER, "Number", NONE, NONE) \
 \
-	X(BEAT, "Beat") \
-	X(REST, "Rest") \
-	X(RAND, "Rand") \
+	X(BEAT, "Beat", NONE, NONE) \
+	X(REST, "Rest", NONE, NONE) \
+	X(RAND, "Rand", NONE, NONE) \
 \
-	X(ADD, "Add") \
-	X(SUB, "Sub") \
-	X(MUL, "Mul") \
-	X(DIV, "Div") \
+	X(ABS, "Abs", NONE, NONE) \
+	X(NEG, "Neg", NONE, NONE) \
 \
-	X(EUC, "Euc") \
-	X(ASSIGN, "Assign") \
+	X(ADD, "Add", NONE, NONE) \
+	X(SUB, "Sub", NONE, NONE) \
+	X(MUL, "Mul", NONE, NONE) \
+	X(DIV, "Div", NONE, NONE) \
 \
-	X(INVERT, "Invert") \
-	X(REVERSE, "Reverse") \
+	X(EUC, "Euc", NONE, NONE) \
+	X(ASSIGN, "Assign", NONE, NONE) \
 \
-	X(SHIFTLEFT, "ShiftLeft") \
-	X(SHIFTRIGHT, "ShiftRight") \
+	X(TIMEMUL, "TimeMul", NONE, NONE) \
+	X(TIMEDIV, "TimeDiv", NONE, NONE) \
 \
-	X(REPEAT, "Repeat") \
-	X(MAP, "Map") \
+	X(INVERT, "Invert", NONE, NONE) \
+	X(REVERSE, "Reverse", NONE, NONE) \
 \
-	X(OR, "Or") \
-	X(AND, "And") \
-	X(XOR, "Xor") \
+	X(SHIFTLEFT, "ShiftLeft", NONE, NONE) \
+	X(SHIFTRIGHT, "ShiftRight", NONE, NONE) \
 \
-	X(BRACKETLEFT, "BracketLeft") \
-	X(BRACKETRIGHT, "BracketRight") \
+	X(REPEAT, "Repeat", NONE, NONE) \
+	X(MAP, "Map", NONE, NONE) \
 \
-	X(PARENLEFT, "ParenLeft") \
-	X(PARENRIGHT, "ParenRight") \
+	X(LCM, "Lcm", NONE, NONE) \
+	X(GCD, "Gcd", NONE, NONE) \
 \
-	X(END, "End")
+	X(OR, "Or", NONE, NONE) \
+	X(AND, "And", NONE, NONE) \
+	X(XOR, "Xor", NONE, NONE) \
+\
+	X(BRACKETLEFT, "BracketLeft", NONE, NONE) \
+	X(BRACKETRIGHT, "BracketRight", NONE, NONE) \
+\
+	X(PARENLEFT, "ParenLeft", NONE, NONE) \
+	X(PARENRIGHT, "ParenRight", NONE, NONE) \
+\
+	X(BRACELEFT, "BraceLeft", NONE, NONE) \
+	X(BRACERIGHT, "BraceRight", NONE, NONE) \
+\
+	X(END, "End", NONE, NONE)
 
-#define X(a, b) a,
+#define X(sym, str, prec, ass) sym,
 	enum class SymbolKind : size_t {
-		SYMBOL_KINDS
+		SYMBOL_KINDS TOTAL,
 	};
 #undef X
 
 	namespace detail {
-#define X(a, b) std::string_view { b },
+#define X(sym, str, prec, ass) std::string_view { str },
 		constexpr std::array SYMBOL_TO_STR = { SYMBOL_KINDS };
 #undef X
 
@@ -326,6 +356,50 @@ namespace cane {
 	inline std::ostream& operator<<(std::ostream& os, SymbolKind x) {
 		return (os << detail::symbol_to_str(x));
 	}
+
+	namespace detail {
+#define X(sym, str, prec, ass) table.at(static_cast<size_t>(SymbolKind::sym)) = { PrecKind::prec, AssKind::ass };
+
+		constexpr decltype(auto) generate_precedence_table() {
+			std::array<std::pair<PrecKind, AssKind>, static_cast<size_t>(SymbolKind::TOTAL)> table;
+			table.fill({ PrecKind::NONE, AssKind::NONE });
+
+			SYMBOL_KINDS
+
+			std::array<std::pair<size_t, size_t>, static_cast<size_t>(SymbolKind::TOTAL)> lookup;
+
+			size_t i = 0;
+			size_t running = 0;
+
+			for (auto [prec, ass]: table) {
+				size_t assoc = 0;
+				size_t prece = 0;
+
+				switch (prec) {
+					case PrecKind::NONE: prece = 0; break;
+					case PrecKind::LAST: prece = running; break;
+					case PrecKind::INCR: prece = ++running; break;
+
+					default: break;
+				}
+
+				switch (ass) {
+					case AssKind::RIGHT:
+					case AssKind::NONE: assoc = 0; break;
+					case AssKind::LEFT: assoc = 1; break;
+
+					default: break;
+				}
+
+				lookup.at(i++) = { prece, prece + assoc };
+			}
+
+			return lookup;
+		}
+
+#undef X
+	}  // namespace detail
+
 }  // namespace cane
 
 template <>
@@ -407,7 +481,9 @@ namespace cane {
 		Symbol peek_;
 
 		public:
-		Lexer(std::string_view sv): sv_(sv), sv_it_(sv_.begin()), sv_end_(sv_.end()), peek_() {}
+		Lexer(std::string_view sv): sv_(sv), sv_it_(sv_.begin()), sv_end_(sv_.end()), peek_() {
+			[[maybe_unused]] Symbol s = take();
+		}
 
 		template <typename F>
 		[[nodiscard]] constexpr bool take_if(F&& fn) {  // Take character if it matches predicate.
@@ -456,6 +532,14 @@ namespace cane {
 
 			else if (sym.sv == "rev") {
 				sym.kind = SymbolKind::REVERSE;
+			}
+
+			else if (sym.sv == "lcm") {
+				sym.kind = SymbolKind::LCM;
+			}
+
+			else if (sym.sv == "gcd") {
+				sym.kind = SymbolKind::GCD;
 			}
 
 			return true;
@@ -545,6 +629,14 @@ namespace cane {
 				sym.kind = SymbolKind::PARENRIGHT;
 			}
 
+			else if (take_str("{")) {
+				sym.kind = SymbolKind::BRACELEFT;
+			}
+
+			else if (take_str("}")) {
+				sym.kind = SymbolKind::BRACERIGHT;
+			}
+
 			else {
 				return false;
 			}
@@ -565,18 +657,11 @@ namespace cane {
 			return true;
 		}
 
-		template <typename F>
-		constexpr void expect(F&& fn, ErrorKind x) {
-			if (not fn(peek_)) {
-				report(peek_.sv, x);
-			}
-		}
-
 		[[nodiscard]] constexpr Symbol peek() const {
 			return peek_;
 		}
 
-		[[nodiscard]] constexpr bool take(Symbol& out) {
+		[[nodiscard]] constexpr Symbol take() {
 			Symbol sym;
 
 			take_whitespace(sym);  // Skip whitespace.
@@ -590,48 +675,286 @@ namespace cane {
 			else if (take_sigil(sym)) {}
 
 			else {
-				return false;
+				report(sym.sv, ErrorKind::UNEXPECTED_CHAR);
 			}
 
-			out = peek_;
+			Symbol out = peek_;
 			peek_ = sym;
 
-			return true;
+			return out;
+		}
+
+		template <typename F>
+		constexpr void expect(F&& fn, ErrorKind x) {
+			if (not fn(peek_)) {
+				report(peek_.sv, x);
+			}
+		}
+
+		template <typename F>
+		[[nodiscard]] constexpr Symbol take_or(F&& fn, ErrorKind x) {
+			if (not fn(peek_)) {
+				report(peek_.sv, x);
+			}
+
+			return take();
 		}
 	};
 
 }  // namespace cane
 
-int main(int, const char* argv[]) {
-	try {
-		cane::Symbol s;
-		cane::Lexer lx { std::string_view { CANE_DBG(argv[1]) } };
+namespace cane {
+	// Token types
+#define OP_KINDS \
+	X(PREFIX, "Prefix") \
+	X(INFIX, "Infix") \
+	X(POSTFIX, "Postfix")
 
-		if (not lx.take(s)) {
-			cane::report(s.sv, cane::ErrorKind::UNKNOWN_CHAR);
+#define X(a, b) a,
+	enum class OpKind : size_t {
+		OP_KINDS
+	};
+#undef X
+
+	namespace detail {
+#define X(a, b) std::string_view { b },
+		constexpr std::array OP_TO_STR = { OP_KINDS };
+#undef X
+
+		constexpr std::string_view op_to_str(OpKind x) {
+			return detail::OP_TO_STR[static_cast<size_t>(x)];
+		}
+	}  // namespace detail
+
+	inline std::ostream& operator<<(std::ostream& os, OpKind x) {
+		return (os << detail::op_to_str(x));
+	}
+}  // namespace cane
+
+template <>
+struct fmt::formatter<cane::OpKind>: fmt::ostream_formatter {};
+
+namespace cane {
+	constexpr bool is_primary(Symbol s) {
+		return eq_any(s.kind,
+			SymbolKind::IDENTIFIER,
+			SymbolKind::NUMBER,
+			SymbolKind::BRACKETLEFT,
+			SymbolKind::PARENLEFT,
+			SymbolKind::BEAT,
+			SymbolKind::REST,
+			SymbolKind::RAND);
+	}
+
+	constexpr bool is_prefix(Symbol s) {
+		return eq_any(s.kind,
+			SymbolKind::INVERT,
+			SymbolKind::REVERSE,
+			SymbolKind::ADD,  // Abs
+			SymbolKind::SUB,  // Neg
+			SymbolKind::MUL,  // Tempo Mul
+			SymbolKind::DIV   // Tempo Div
+		);
+	}
+
+	constexpr bool is_infix(Symbol s) {
+		return eq_any(s.kind,
+			SymbolKind::ADD,
+			SymbolKind::SUB,
+			SymbolKind::MUL,
+			SymbolKind::DIV,
+			SymbolKind::EUC,
+			SymbolKind::MAP,
+			SymbolKind::REPEAT,
+			SymbolKind::SHIFTLEFT,
+			SymbolKind::SHIFTRIGHT,
+			SymbolKind::OR,
+			SymbolKind::AND,
+			SymbolKind::XOR);
+	}
+
+	constexpr bool is_postfix(Symbol s) {
+		return eq_any(s.kind, SymbolKind::ASSIGN);
+	}
+
+	using Tree = std::vector<Symbol>;
+
+	inline std::pair<size_t, size_t> binding_power(Symbol s, OpKind fix) {
+		auto [sv, kind] = s;
+
+		size_t LEFT = 1;
+		size_t RIGHT = 0;
+
+		// Precedence. ascending order
+		enum : size_t {
+			MAP,
+			ASSIGN = MAP,
+
+			OR,
+			AND = OR,
+			XOR = OR,
+			REPEAT = OR,
+			SHIFTLEFT = OR,
+			SHIFTRIGHT = OR,
+
+			INVERT,
+			REVERSE = INVERT,
+
+			ADD,
+			SUB = ADD,
+
+			MUL,
+			DIV = MUL,
+
+			TIMEMUL,
+			TIMEDIV = TIMEMUL,
+
+			EUC,
+
+			LCM,
+			GCD = LCM,
+
+			POS,
+			NEG = POS,
+		};
+
+		switch (fix) {
+			case OpKind::PREFIX:
+				switch (kind) {
+					case SymbolKind::ADD: return { 0u, POS + RIGHT };  // Pos
+					case SymbolKind::SUB: return { 0u, NEG + RIGHT };  // Neg
+
+					case SymbolKind::MUL: return { 0u, TIMEMUL + RIGHT };  // Time Mul  `* 2 !!!!`
+					case SymbolKind::DIV: return { 0u, TIMEDIV + RIGHT };  // Time Div  `/ 2 !!!!`
+
+					case SymbolKind::INVERT: return { 0u, INVERT + RIGHT };    // Invert
+					case SymbolKind::REVERSE: return { 0u, REVERSE + RIGHT };  // Reverse
+
+					default: break;
+				}
+
+				break;
+
+			case OpKind::INFIX:
+				switch (kind) {
+					case SymbolKind::ADD: return { ADD, ADD + LEFT };
+					case SymbolKind::SUB: return { SUB, SUB + LEFT };
+					case SymbolKind::MUL: return { MUL, MUL + LEFT };
+					case SymbolKind::DIV: return { DIV, DIV + LEFT };
+
+					case SymbolKind::OR: return { OR, OR + LEFT };
+					case SymbolKind::AND: return { AND, AND + LEFT };
+					case SymbolKind::XOR: return { XOR, XOR + LEFT };
+
+					case SymbolKind::SHIFTLEFT: return { SHIFTLEFT, SHIFTLEFT + LEFT };
+					case SymbolKind::SHIFTRIGHT: return { SHIFTRIGHT, SHIFTRIGHT + LEFT };
+
+					case SymbolKind::LCM: return { LCM, LCM + LEFT };
+					case SymbolKind::GCD: return { GCD, GCD + LEFT };
+
+					case SymbolKind::EUC: return { EUC, EUC + LEFT };
+					case SymbolKind::REPEAT: return { REPEAT, REPEAT + LEFT };
+					case SymbolKind::MAP: return { MAP, MAP + LEFT };
+
+					default: break;
+				}
+
+				break;
+
+			case OpKind::POSTFIX:
+				switch (kind) {
+					case SymbolKind::ASSIGN: return { ASSIGN, ASSIGN + LEFT };
+					default: break;
+				}
+
+				break;
+
+			default: break;
 		}
 
-		while (lx.peek().kind != cane::SymbolKind::ENDFILE) {
-			if (not lx.take(s)) {
-				cane::report(s.sv, cane::ErrorKind::UNKNOWN_CHAR);
+		return { 0u, 0u };
+	}
+
+	constexpr void primary(Lexer& lx, Tree& tree, size_t bp) {
+		// Symbol sym = lx.peek();
+
+		// switch (sym.kind) {
+		// 	case Symbols::
+		// }
+	}
+
+	constexpr void prefix(Lexer& lx, Tree& tree, size_t bp) {}
+
+	constexpr void infix(Lexer& lx, Tree& tree, size_t bp) {}
+
+	constexpr void postfix(Lexer& lx, Tree& tree, size_t bp) {}
+
+	constexpr void expression(Lexer& lx, Tree& tree, size_t bp) {
+		Symbol sym = lx.peek();
+
+		if (is_prefix(sym)) {  // prefix expression
+			auto [lbp, rbp] = binding_power(sym, OpKind::PREFIX);
+			prefix(lx, tree, rbp);
+		}
+
+		else if (is_primary(sym)) {  // primary expr
+			primary(lx, tree, 0);
+		}
+
+		else {
+			report(sym.sv, ErrorKind::UNEXPECTED_TOKEN);
+		}
+
+		sym = lx.peek();
+
+		while (is_infix(sym) or is_postfix(sym)) {  // infix or postfix
+			if (is_postfix(sym)) {
+				auto [lbp, rbp] = binding_power(sym, OpKind::POSTFIX);
+				if (lbp < bp) {
+					break;
+				}
+
+				postfix(lx, tree, 0);
 			}
 
-			CANE_LOG(cane::LogKind::OKAY, "{}", s);
+			else if (is_infix(sym)) {
+				auto [lbp, rbp] = binding_power(sym, OpKind::INFIX);
+				if (lbp < bp) {
+					break;
+				}
+
+				infix(lx, tree, rbp);
+			}
+
+			else {
+				report(sym.sv, ErrorKind::UNEXPECTED_TOKEN);
+			}
+
+			sym = lx.peek();
 		}
-		// lx.expect(cane::is_sym(cane::SymbolKind::NUMBER), cane::ErrorKind::EXPECTED_NUMBER);
+	}
+}  // namespace cane
 
-		// if (not CANE_DBG(lx.take_ident(s))) {
-		// 	cane::report(s.sv, cane::ErrorKind::EXPECTED_IDENT);
+int main(int, const char* argv[]) {
+	try {
+		cane::Lexer lx { std::string_view { argv[1] } };
+
+		// [[maybe_unused]] cane::Symbol s;
+
+		// s = CANE_DBG(lx.take_or(cane::is_sym(cane::SymbolKind::NUMBER), cane::ErrorKind::EXPECTED_NUMBER));
+		// s = CANE_DBG(lx.take_or(cane::is_sym(cane::SymbolKind::ADD), cane::ErrorKind::EXPECTED_OPERATOR));
+		// s = CANE_DBG(lx.take_or(cane::is_sym(cane::SymbolKind::NUMBER), cane::ErrorKind::EXPECTED_NUMBER));
+
+		// lx.expect(s, cane::is_sym(cane::SymbolKind::IDENTIFIER), cane::ErrorKind::EXPECTED_IDENTIFIER);
+		// lx.take();
+
+		// while (lx.peek().kind != cane::SymbolKind::ENDFILE) {
+		// 	if (not lx.take(s)) {
+		// 		cane::report(s.sv, cane::ErrorKind::UNKNOWN_CHAR);
+		// 	}
+
+		// 	CANE_LOG(cane::LogKind::OKAY, "{}", s);
 		// }
-
-		// CANE_DBG(lx.take_whitespace(s));
-
-		// if (not CANE_DBG(lx.take_number(s))) {
-		// 	cane::report(s.sv, cane::ErrorKind::EXPECTED_NUMBER);
-		// }
-
-		// CANE_DBG(lx.take_sigil(s));
-		// CANE_LOG(cane::LogKind::EXPR, "{}", s);
 	}
 
 	catch (cane::Report r) {
