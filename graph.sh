79602f6422a701f04b4bbf102b6b56dd77d69d62
Jack Clarke <jackojc@gmail.com>
2025-10-08 00:20:10 +0100

    Fix graph.sh

--- graph.sh
+++ graph.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-meson compile -C build
-./build/cane
-dot -Tpng cane.dot -o cane.png
+meson compile -C build && \
+./build/cane && \
+dot -Tpng cane.dot -o cane.png && \
 nsxiv cane.png
